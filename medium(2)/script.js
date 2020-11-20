b0ae043d2fd65c62663204527d9483fe7a202c90
cfor0 <cfor0@outlook.com>
2020-11-20 16:46:32 -0500

    finishing touches on med 2 exercise

--- medium(2)/script.js
+++ medium(2)/script.js
@@ -4,35 +4,41 @@ let string2 = "Madonna of the Rocks";
 
 let anagram = (a, b) => {
     // Determine wheter they are an anagram 
-    let isAnagram = false;
+    let isAnagram = true;
     // Use regex to get rid of spaces
     let result1 = a.replace(/ /g, '').toLowerCase()
-    
+
     // Use split method to give each letter its own index
     let firstArr = result1.split('');
     firstArr.sort();
 
     let result2 = b.replace(/ /g, '').toLowerCase()
-    
+
     let secArr = result2.split('');
     secArr.sort();
 
     if (firstArr.length === secArr.length) {
         for (let i = 0; i < firstArr.length; i++) {
-            if(firstArr[i] === secArr[i]) {
-                isAnagram = true;
-                return console.log(isAnagram);
+            if (firstArr[i] === secArr[i] && isAnagram === true) {
+                console.log(isAnagram)
+            } else if (firstArr[i] != secArr[i]) {
+                isAnagram = false
+               return console.log(isAnagram)
+            } else {
+                isAnagram = true
+                console.log(isAnagram)
             }
         }
     } else {
+        isAnagram = false
         return console.log(isAnagram);
     }
 
 
-    
 
-    console.log(firstArr)
-    console.log(secArr)
+
+    // console.log(firstArr)
+    // console.log(secArr)
 
 }
 
